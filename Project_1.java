03e920b8416c84123cf41373d6714b7df78b88f2
elliotvm <68871718+elliotvm@users.noreply.github.com>
2024-01-27 19:56:36 -0500

    Add files via upload

---
+++ Project_1.java
@@ -0,0 +1,79 @@
+
+import java.util.Arrays;
+
+
+
+public class Project_1 {
+
+    public static void main(String[] args) {
+        int[] A = {1, 4, 3, 6, 8, 2, 5, 9, 7};
+        int[] B = QuickSortDoublePointer(A, 0, 8);
+        int[] C = QuickSortSinglePointer(A, 0, 8);
+        
+        System.out.println(Arrays.toString(B));
+        System.out.println(Arrays.toString(C));
+    }
+    
+    public static void swap(int[] A, int lower, int upper){
+        int x = A[lower];
+        A[lower] = A[upper];
+        A[upper] = x;
+    }
+    
+    public static int DoublePointerPartition(int[] A, int first, int last){
+        int pivot = A[last];
+        int lower = first;
+        int upper = last-1;
+        
+        while(lower <= upper){
+            while(lower <= upper && A[upper] >= pivot){
+                upper--;
+            }
+            while(lower <= upper && A[lower] <= pivot){
+                lower++;
+            }
+            if(lower < upper){
+                swap(A, lower, upper);
+            }            
+        }
+        
+        swap(A, lower, last);
+        return lower;
+        
+    }
+    
+    public static int SinglePointerPartition(int[] A, int first, int last){
+        int pivot = A[last];
+        int lowPointer = first - 1;
+        
+        for(int pointer = first; pointer <= last - 1; pointer++){
+            if(A[pointer] <= pivot){
+                lowPointer = lowPointer + 1;
+                swap(A, lowPointer, pointer);
+            }
+        }
+        
+        lowPointer = lowPointer + 1;
+        swap(A, lowPointer, last);
+        return lowPointer;
+    }
+    
+    public static int[] QuickSortDoublePointer(int[] A, int first, int last){
+        if(first<last){
+            int mid = DoublePointerPartition(A, first, last);
+            QuickSortDoublePointer(A, first, mid-1);
+            QuickSortDoublePointer(A, mid+1, last);
+        }
+        return A;
+    }
+    
+    public static int[] QuickSortSinglePointer(int[] A, int first, int last){
+        if(first<last){
+            int mid = SinglePointerPartition(A, first, last);
+            QuickSortSinglePointer(A, first, mid-1);
+            QuickSortSinglePointer(A, mid+1, last);
+        }
+        return A;
+    }    
+    
+}
