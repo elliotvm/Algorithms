dad975e56a6f33e97623046deb48516ae51d1c27
KennethLee3 <73751512+KennethLee3@users.noreply.github.com>
2024-02-24 23:06:20 -0500

    Update project_2.java
    Recursive without memoization works correctly now.

--- Project2/project_2.java
+++ Project2/project_2.java
@@ -1,3 +1,5 @@
+package com.mycompany.project_2;
+
 import java.util.*;
 
 public class Project_2 {
@@ -23,7 +25,7 @@ public class Project_2 {
                 p = p - c;
             }   
             
-            for(int k = 0; k < denomination.length; k++){
+            for(int k = denomination.length - 1; k >= 0; k--){
                 if(counts[k] != 0){
                     System.out.print(denomination[k] + ":" + counts[k] + " ");
                 }
@@ -32,6 +34,27 @@ public class Project_2 {
         }
     }
     
+    public static void printResultRecursive(int[] denomination, Node head) {
+        int counts[] = new int[denomination.length];
+        System.out.print(head.value + " cents = ");
+
+        while (head.prevNode != null) {
+            for(int j = 0; j < denomination.length; j++){
+                if(denomination[j] == head.value - head.prevNode.value){
+                    counts[j]++;
+                }
+            }
+            head = head.prevNode;
+        }
+        
+        for (int k = denomination.length - 1; k >= 0; k--) {
+            if (counts[k] != 0) {
+                System.out.print(denomination[k] + ":" + counts[k] + " ");
+            }
+        }
+        System.out.println();
+    }
+     
     public static void bottomUp(int[] denomination, int[] targets){
         
         int size = 0;
@@ -79,8 +102,42 @@ public class Project_2 {
 
     }
     
-    public static void main(String[] args) {
+    public static void pureRecursive( int[] denomination, int[] targets) {
+        Node table[] = new Node[targets.length];
         
+        for (int i = 0; i < targets.length; i++) {
+            if (targets[i] < 100) {
+                Node prev = pureRecursiveCall(denomination, targets[i]);
+                table[i] = new Node(prev.numCoins + 1, targets[i], prev);
+                printResultRecursive(denomination, table[i]);
+            }
+        }
+    }
+    
+    public static Node pureRecursiveCall( int[] denomination, int target) {
+        Node bestNode = new Node(10000, 0, null);
+        for (int j = denomination.length - 1; j >= 0; j--) {
+            if (target - denomination[j] == 0) {
+                bestNode = new Node(0, 0, null);
+            }
+            else if (target - denomination[j] < 0) {
+            }
+            else {
+                Node currNode = pureRecursiveCall(denomination, target - denomination[j]);
+                currNode.prevNode = new Node(currNode);
+                currNode.numCoins++;
+                currNode.value = target - denomination[j];
+                if (currNode.numCoins < bestNode.numCoins) {
+                    bestNode = currNode;
+                }
+            }
+        }
+        //System.out.println(bestNode.numCoins + " " + bestNode.value);
+        return bestNode;
+    }
+    
+    public static void main(String[] args) {
+
         //parse input
         Scanner in = new Scanner(System.in);
 
@@ -98,6 +155,7 @@ public class Project_2 {
             targets[i] = in.nextInt();
         }
 
+        pureRecursive(denomination, targets);
         bottomUp(denomination, targets);
     }
 }
