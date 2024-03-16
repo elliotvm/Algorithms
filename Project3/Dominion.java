1f1abafc14544943ef784136ba7424fb7594a0d5
elliotvm <68871718+elliotvm@users.noreply.github.com>
2024-03-15 20:25:43 -0400

    Create Dominion.java

---
+++ Project3/Dominion.java
@@ -0,0 +1,11 @@
+public class Dominion {
+    int location;
+    Dominion parent;
+    int rank;
+    
+    public Dominion(int l){
+        location = l;
+        parent = null;
+        rank = 0;
+    }
+}
