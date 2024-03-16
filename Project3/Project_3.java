dc6bf7096754ebb53bd4e67e5fa3a6f9152ae35b
elliotvm <68871718+elliotvm@users.noreply.github.com>
2024-03-15 20:20:21 -0400

    Update Project_3.java

--- Project3/Project_3.java
+++ Project3/Project_3.java
@@ -4,6 +4,45 @@ import java.util.*;
 
 public class Project_3 {
 
+    
+    void makeSet(Dominion x, List<Dominion> empire){
+        empire.add(x);
+        x.parent = x;
+        x.rank = 0;
+    }
+    
+        
+    Dominion findSet(Dominion x){//returns the representative dominion
+        if(x != x.parent){
+            x.parent = findSet(x.parent);
+        }
+        return x.parent;
+    }
+    
+    void union(Dominion x, Dominion y, List<Dominion> empire){
+        
+        Dominion repX = findSet(x);
+        Dominion repY = findSet(y);
+        
+        if(repX.rank >= repY.rank){
+            empire.remove(y);
+            repY.parent = repX;
+            repX.rank += 1;
+        }else{
+            empire.remove(x);
+            repX.parent = repY;
+            repY.rank += 1;
+        }
+    }
+
+    boolean sameComponent(Dominion u, Dominion v){
+        return findSet(u) == findSet(v);
+    }
+    
+    void connectedComponents(Dominion u, Dominion v){
+        
+    }
+    
     public static void main(String[] args) {
         
         Scanner in = new Scanner(System.in);
