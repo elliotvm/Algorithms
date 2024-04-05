125e6a5f6d7113f0f8b2320446ee9fde1835a2b6
elliotvm <68871718+elliotvm@users.noreply.github.com>
2024-04-05 19:25:06 -0400

    Create Vehicle.java

---
+++ Project4/Vehicle.java
@@ -0,0 +1,12 @@
+public class Vehicle {
+    String type;
+    String color;
+    String orientation;
+    
+    public Vehicle(String t, String c, String o){
+        type = t;
+        color = c;
+        orientation = o;
+    }
+    
+}
