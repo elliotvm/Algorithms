ae2ac58405f686334de4ad944da3963270e1cdfd
elliotvm <68871718+elliotvm@users.noreply.github.com>
2024-04-05 21:18:00 -0400

    Update Move.java

--- Project4/Move.java
+++ Project4/Move.java
@@ -1,8 +1,14 @@
+package elliot.project_4;
+
 public class Move {
     int board[][] = new int[6][6];
     String move;
     
     Move(int[][] b, String c, int n, char d){
+        board = b;
         move = c + " " + Integer.toString(n) + " " + d;
+    }
+    Move(int[][] b){
+        board = b;
     }
 }
