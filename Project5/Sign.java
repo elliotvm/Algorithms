f621d622c814503eb9a71d2b5fd5981d89ffc7a6
elliotvm <68871718+elliotvm@users.noreply.github.com>
2024-04-21 20:55:23 -0400

    Create Sign.java

---
+++ Project5/Sign.java
@@ -0,0 +1,11 @@
+public class Sign {
+    int start;
+    int end;
+    double distance;
+    
+    public Sign(int s, int e, double d){
+        start = s;
+        end = e;
+        distance = d;
+    }
+}
