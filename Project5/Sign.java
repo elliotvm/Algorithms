5746aac46aba1f996a9cf74b7a8f085dcbad5ff1
KennethLee3 <73751512+KennethLee3@users.noreply.github.com>
2024-04-21 22:00:57 -0400

    Update Sign.java

--- Project5/Sign.java
+++ Project5/Sign.java
@@ -1,11 +1,18 @@
+import java.util.*;
+
 public class Sign {
     int start;
     int end;
     double distance;
+    List<City> cities = new ArrayList<>();
     
     public Sign(int s, int e, double d){
         start = s;
         end = e;
         distance = d;
+    }
+    
+    public void addCity(City c) {
+        cities.add(c);
     }
 }
